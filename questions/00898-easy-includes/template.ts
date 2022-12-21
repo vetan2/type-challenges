c990475c37c1df2cb5f3f61bf202f8fe51b49b9e
vetan2 <vetan2@naver.com>
2022-12-21 10:29:16 +0900

    00898

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,11 @@
-type Includes<T extends readonly any[], U> = any
+type Equal<A, B> = (<T>() => T extends A ? 1 : 2) extends <T>() => T extends B
+  ? 1
+  : 2
+  ? true
+  : false;
+
+type Includes<T extends readonly any[], U> = {
+  [Key in keyof T]: Equal<U, T[Key]>;
+}[number] extends false
+  ? false
+  : true;
